d7093e38e4f4e8992a060a76b374721838cbac91
krishnakumar.m <krishnakumar.m@endurance.com>
2020-04-19 01:16:43 +0530

    minimal implementation of CRD operations (out of CRUD)

--- app.js
+++ app.js
@@ -1,26 +1,87 @@
 const express = require('express')
-const hbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 const path = require('path')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const redis = require('redis')
 
+// Create Redis client
+const client = redis.createClient()
+client.on('connect', function () {
+    console.log('Conncted to Redis')
+})
+
 // Set port
 const port = 3000
+
 // Init app
 const app = express()
+
 // View engine
-app.engine('hbs', hbs({defaultlayout: 'main'}))
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs({defaultlayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 // body-parser
  app.use(bodyParser.json())
- app.use(bodyParser.urlencoded())
+ app.use(bodyParser.urlencoded({ extended: true }))
 
 // methodOverride
 app.use(methodOverride('_method'))
 
-app.get('/', function (req, res){
-    res.render('searchuser s')
+// Search page
+app.get('/', function (req, res, next) {
+    res.render('searchusers')
+})
+
+// Search processing
+app.post('/users/search', function (req, res, next) {
+ const id = req.body.id
+
+ client.hgetall('users:' + id, function (err, obj) {
+     if (!obj) {
+         res.render('searchusers', {
+             error: 'User does not exist'
+         })
+     } else {
+         obj.id = id
+         res.render('details', {
+             user: obj
+         })
+     }
+ })
+})
+
+//  Get Add user page
+app.get('/users/add', function (req, res, next) {
+    res.render('adduser')
+})
+
+// Process Add user
+app.post('/users/add', function (req, res, next) {
+    const id = req.body.id
+    const firstName = req.body.first_name
+    const lastName = req.body.last_name
+    const email = req.body.email
+    const phone = req.body.phone
+
+    client.hset ('users:' + id, [
+        'first_name', firstName,
+        'last_name', lastName,
+        'email', email,
+        'phone', phone
+    ], function (err, reply) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log(reply)
+        res.redirect('/')
+    })
+})
+
+// Process Delete user
+app.delete('/users/:id', function (req, res, next) {
+    client.del('users:' + req.params.id)
+    res.redirect('/')
 })
 
 app.listen(port, function () {
